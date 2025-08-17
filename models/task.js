36a1c2e994ecea555b37ce015abfab0f00c759b5
Naseer Ahmed S <119148927+naseerahmeds@users.noreply.github.com>
2025-08-17 17:23:43 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
-  status: { type: String, enum: ["To-Do", "Completed"], default: "To-Do" },
+  status: { type: String, enum: ["To-Do-Now", "Completed"], default: "To-Do" },
   createdAt: { type: Date, default: Date.now },
 });
 
